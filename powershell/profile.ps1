bc780b9883b4d1eb66ac88c071343bec7346c762
Raphael Fleischlin <git@raefftec.ch>
2020-06-16 20:09:08 +0200

    add powershell profile

---
+++ powershell/profile.ps1
@@ -0,0 +1,22 @@
+
+Import-Module posh-git
+Import-Module oh-my-posh
+
+Function Open-VsCode {
+  Param(
+    [string]$path="."
+  )
+
+  code $path
+}
+
+Function Open-Explorer {
+  Param(
+    [string]$path="."
+  )
+
+  explorer $path
+}
+
+Set-Alias -Name c -Value Open-VsCode
+Set-Alias -Name e -Value Open-Explorer

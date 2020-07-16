a0c1ee19062f312ffc162a4e0c4120bf0111bb41
Raphael Fleischlin <git@raefftec.ch>
2020-07-16 17:20:49 +0200

    link windows terminal config

--- windows-terminal/configure.ps1
+++ windows-terminal/configure.ps1
@@ -3,4 +3,9 @@
 Add-Package 'microsoft-windows-terminal' -displayName 'Windows Terminal'
 
 # verify
-(Get-AppxPackage -Name Microsoft.WindowsTerminal).Version | AddVersion -name 'Windows Terminal'
+$packageInfo = Get-AppxPackage -Name Microsoft.WindowsTerminal
+$packageInfo.Version | AddVersion -name 'Windows Terminal'
+
+# link settings
+$settings = "$($env:LOCALAPPDATA)\packages\$($packageInfo.PackageFamilyName)\LocalState\settings.json"
+New-Item -ItemType SymbolicLink -Path $settings -Target "$PSScriptRoot\settings.json" -Force

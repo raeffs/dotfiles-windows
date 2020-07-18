17677add3197f57e147d47548d3ce2fb0f24008b
Raphael Fleischlin <git@raefftec.ch>
2020-07-18 17:37:40 +0200

    add new aliases

---
+++ powershell/aliases.ps1
@@ -0,0 +1,31 @@
+
+# navigation aliases
+Function .. { Set-Location .. }
+Function ... { Set-Location ..\.. }
+Function .... { Set-Location ..\..\.. }
+Function home { Set-Location ~ }
+
+# list all files
+Function la { ls -Force @args }
+# list directories
+Function lsd { ls -Directory -Force @args }
+
+# open explorer
+Function Open-Explorer {
+  Param(
+    [string]$path='.'
+  )
+
+  explorer $path
+}
+Set-Alias -Name e -Value Open-Explorer
+
+# open visual studio code
+Function Open-VsCode {
+  Param(
+    [string]$path='.'
+  )
+
+  code $path
+}
+Set-Alias -Name c -Value Open-VsCode
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,29 +1,13 @@
 
+# we need to resolve the symbolic link in order to get the correct path for the profile
+$profileDir = Split-Path -parent (Get-Item $PROFILE).Target
+
 Import-Module posh-git
 
 Import-Module oh-my-posh
-# we need to resolve the symbolic link in order to get the correct path for the themes folder
-$profileDir = Split-Path -parent (Get-Item $PROFILE).Target
 $ThemeSettings.MyThemesLocation = "$profileDir\themes"
 Set-Theme custom-theme
 
-Function Open-VsCode {
-  Param(
-    [string]$path="."
-  )
-
-  code $path
-}
-
-Function Open-Explorer {
-  Param(
-    [string]$path="."
-  )
-
-  explorer $path
-}
-
-Set-Alias -Name c -Value Open-VsCode
-Set-Alias -Name e -Value Open-Explorer
+. $profileDir\aliases.ps1
 
 Clear-Host

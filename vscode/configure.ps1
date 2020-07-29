ce8e347f9e1a90e325b429ab54f6b9ce9f8a5240
Raphael Fleischlin <git@raefftec.ch>
2020-07-29 17:55:39 +0200

    add vscode config

--- vscode/configure.ps1
+++ vscode/configure.ps1
@@ -1,3 +1,10 @@
 
 # install & update
 Add-Package 'vscode' -displayName 'Visual Studio Code' -params '/NoDesktopIcon'
+
+# verify
+code --version | Select-Object -First 1 | AddVersion -name 'Visual Studio Code'
+
+# link settings
+New-Item -ItemType SymbolicLink -Path "$($env:APPDATA)\Code\User\settings.json" -Target "$PSScriptRoot\settings.json" -Force
+New-Item -ItemType SymbolicLink -Path "$($env:APPDATA)\Code\User\keybindings.json" -Target "$PSScriptRoot\keybindings.json" -Force

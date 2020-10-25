49a14be7ecb51a4a522375e8495f42ebeb003179
Raphael Fleischlin <git@raefftec.ch>
2020-10-25 09:20:44 +0100

    install vscode only if not already installed, closes #20

--- vscode/configure.ps1
+++ vscode/configure.ps1
@@ -1,6 +1,10 @@
 
 # install & update
-Add-Package 'vscode' -displayName 'Visual Studio Code' -params '/NoDesktopIcon'
+if (Get-CommandSave 'code') {
+    Write-Message "Visual Studio Code is already installed!"
+} else {
+    Add-Package 'vscode' -displayName 'Visual Studio Code' -params '/NoDesktopIcon'
+}
 
 # verify
 code --version | Select-Object -First 1 | AddVersion -name 'Visual Studio Code'

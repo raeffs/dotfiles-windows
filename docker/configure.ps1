42ee700efa855403373b0f00f33175fdf069dffe
Raphael Fleischlin <git@raeffs.dev>
2021-02-15 16:56:09 +0100

    remove docker version output, close #16

--- docker/configure.ps1
+++ docker/configure.ps1
@@ -1,7 +1,3 @@
 
 # install & update
 Add-Package 'docker-desktop' -displayName 'Docker'
-
-# verify
-docker --version | AddVersion -name 'Docker' -replacement 'docker version'
-docker-compose --version | AddVersion -name 'Docker Compose' -replacement 'docker-compose version'

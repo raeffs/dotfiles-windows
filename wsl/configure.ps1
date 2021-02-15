332fefb5379ccbfe10191e82a93051c78ddcf985
Raphael Fleischlin <git@raeffs.dev>
2021-02-15 16:54:29 +0100

    fix wsl2 installation

--- wsl/configure.ps1
+++ wsl/configure.ps1
@@ -1,15 +1,10 @@
 
-# install wsl2 (package is still under moderation)
-# this will fail if wsl is not yet installed, so a reboot is required, then it can be executed again
+Write-Message "Installing Windows Subsystem for Linux 2 ..."
+
 choco search 'wsl2' --exact --local-only
 if ($LASTEXITCODE -eq 2) {
-    Write-Message "Installing Windows Subsystem for Linux 2 ..."
-    choco install 'wsl2' --version='1.0.0' --yes --limit-output
-    $exitcode = $LASTEXITCODE
-    Write-Message "Exit code $exitcode"
-
-    if ($exitcode -ne 0) {
-        Register-ScheduledTaskAndReboot
-    }
-
+    choco install 'wsl2' --yes --limit-output
+    Register-ScheduledTaskAndReboot
 }
+
+choco install 'wsl2' --yes --limit-output --force
